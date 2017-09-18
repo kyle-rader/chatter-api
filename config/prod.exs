a97af286d0a6654f2c35e1b0edcbbb4c737d6045
Kyle Rader <kyle@kylerader.ninja>
2017-09-17 18:37:39 -0700

    Setup prod config for Postgres

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,13 @@ use Mix.Config
 config :chatter_api, ChatterApi.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :chatter_api, ChatterApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
