2dcee2794e80cf42bdb1f9765bb7d1f195a25387
Kyle Rader <kyle@kylerader.ninja>
2017-09-17 19:07:45 -0700

    Config for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :chatter_api, ChatterApi.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "kr-chatter-api.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
