78bed3bda2b777fd215f1de134fb16d69ab741bf
Kyle Rader <kyle@kylerader.ninja>
2017-09-17 22:09:09 -0700

    Add User model migration

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,6 @@ config :chatter_api, ChatterApi.Endpoint,
   check_origin: false,
   watchers: []
 
-
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule ChatterApi.UserTest do
+  use ChatterApi.ModelCase
+
+  alias ChatterApi.User
+
+  @valid_attrs %{email: "some email", password_hash: "some password_hash"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
